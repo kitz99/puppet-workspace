58847c4e0576004ea09df8e06161a69fcb3a4491
root <root@puppet.codespaces.io>
2018-01-11 18:43:11 +0000

    first lab

--- base.pp
+++ base.pp
@@ -3,13 +3,24 @@ user { 'deploy':
     uid        => 5001,
     home       => '/home/deploy',
     password   => '$1$oSM9T5gg$euty9Irc7y0DLIF/I0K/W1',
-    managehome => true
+    managehome => true,
 }
 
 user { 'rege':
+    ensure     => absent,
+}
+
+$packs =['tree', 'wget', 'git', 'unzip', 'ntp']
+
+package { $packs:
     ensure     => present,
-    uid        => 5002,
-    home       => '/home/rege',
-    password   => '$1$oSM9T5gg$euty9Irc7y0DLIF/I0K/W1',
-    managehome => true
+}
+
+file { '/etc/motd':
+    ensure     => present,
+    content    => "Property of XYZ",
+}
+
+service { 'ntp': 
+    ensure     => running
 }
