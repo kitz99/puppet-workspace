0f716372ca8bac09faab51b3596c9541b0d96eb3
root <root@puppet.codespaces.io>
2018-01-27 15:25:41 +0000

    Create tomcat.conf template using erb

--- code/environments/production/modules/tomcat/manifests/params.pp
+++ code/environments/production/modules/tomcat/manifests/params.pp
@@ -6,4 +6,16 @@ class tomcat::params {
     $packages = [ 'tomcat', 'tomcat-webapps' ]
     $service_name = 'tomcat'
     $service_state = running
+    
+    $catalina_pid = '/var/run/tomcat.pid'
+    $shutdown_verbose = 'false'
+    $security_manager = 'false'
+    $shutdown_wait ='30'
+    $tomcat_fcg_loaded = '1'
+    $java_home = "/usr/lib/jvm/jre"
+    
+    $xms = '64m'
+    $xmx = '128m'
+    $max_perm_size = '128m'
+    
 }
